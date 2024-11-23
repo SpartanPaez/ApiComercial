694b42e3d4f761f06dfe167cbb487e6a4bd40468
Willian Paez <spartanpaez@icloud.com>
2024-11-23 12:04:59 -0300

    AJuste se select de estados, se agrega trim, ya que no permite ver estados que llevan espacios

--- Infraestructure/Repositories/EFVehiculosRepository.cs
+++ Infraestructure/Repositories/EFVehiculosRepository.cs
@@ -130,7 +130,7 @@ namespace ApiComercial.Infraestructure.Repositories
         public async Task<int> GetCountByEstado(string estado)
         {
             return await _my.Vehiculos
-                .Where(v => v.Estado.ToUpper() == estado.ToUpper()) 
+                .Where(v => v.Estado.ToUpper() == estado.ToUpper().Trim()) 
                 .CountAsync();
         }
     }
